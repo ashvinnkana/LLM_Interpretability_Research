3477db917f2920b5b7f8c663626af01bddadbabc
Ashvinn Kanagar <ashvinnkana@gmail.com>
2024-07-23 15:30:07 +0800

    basic text extraction from pdf commit

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+from scripts import extract_file, clean_text
+
+
 def main():
-    return
+    # Extract PDF Contents
+    data = extract_file.extract_pdf('./data/unstructured_data/wa_legislation_files/Limitation_Act_2005.pdf')
+    data = clean_text.basic(data)
+
+    # print content of the file
+    print(data)
 
 
 if __name__ == '__main__':
---
+++ scripts/clean_text.py
@@ -0,0 +1,34 @@
+import re
+
+
+def basic(text):
+    # Remove extra line breaks within sentences
+    text = re.sub(r'\n(?=[a-z])', ' ', text)
+
+    # Remove leading and trailing whitespace
+    text = text.strip()
+
+    # Remove extra spaces
+    # text = re.sub(r'\s+', ' ', text).strip()
+
+    # remove newline before colon
+    text = re.sub(re.compile(r'\n(?=:)'), ' ', text)
+
+    # fix numbering patterns
+    text = re.sub(re.compile(r'(\d)\.\s*\n'), r'\1. ', text)
+
+    # Remove extra spaces between words
+    text = re.sub(r'(?<!\n) +', ' ', text)
+
+    # remove spaces before fullstop
+    text = text.replace(' .', '.')
+
+    # remove spaces between newlines
+    text = text.replace('\n ', '\n')
+
+    # Replace multiple consecutive newlines with a single newline
+    text = re.sub(r'\n\n+', '\n\n', text)
+
+    text = text.replace('\n', '\\n')
+
+    return text
